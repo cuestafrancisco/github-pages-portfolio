9e77da95464f8cb3cbc4b14088f7026d9ae9c456
francisco cuesta <cuestafrancisco@gmail.com>
2020-11-17 22:52:34 -0500

    Updates

---
+++ precache-manifest.c29350a08c4c109e4734197878c81baf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90adc87ab56d30c02ee86110d7726f9",
+    "url": "/github-pages-portfolio/index.html"
+  },
+  {
+    "revision": "a72f9a22edfb682c153a",
+    "url": "/github-pages-portfolio/static/css/2.f1201ff7.chunk.css"
+  },
+  {
+    "revision": "20b3fa9d5fc030c5caf8",
+    "url": "/github-pages-portfolio/static/css/main.4d92f19f.chunk.css"
+  },
+  {
+    "revision": "a72f9a22edfb682c153a",
+    "url": "/github-pages-portfolio/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/github-pages-portfolio/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b3fa9d5fc030c5caf8",
+    "url": "/github-pages-portfolio/static/js/main.1b147bff.chunk.js"
+  },
+  {
+    "revision": "b096e81bbef5a93d3059",
+    "url": "/github-pages-portfolio/static/js/runtime-main.abddcc0d.js"
+  },
+  {
+    "revision": "13cbc420a3975483557030deaaa26fd4",
+    "url": "/github-pages-portfolio/static/media/resume.13cbc420.pdf"
+  }
+]);
